2514a920c033143208cca8515c53978e32070c24
Arman Mamian <armanmamian@gmail.com>
2020-10-28 18:21:25 +0300

    go fmt

--- leetcode/0104_maximum_depth_of_binary_tree/0104.go
+++ leetcode/0104_maximum_depth_of_binary_tree/0104.go
@@ -8,16 +8,16 @@ package leetcode
  *     Right *TreeNode
  * }
  */
- func maxDepth(root *TreeNode) int {
-    if root == nil {
-        return 0
-    }
-    return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
